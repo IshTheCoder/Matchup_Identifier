d435b476fd8259cf78803cddb9d0f5d3c95089fd
Abhijit Brahme <abhijitbrahme@Abhijits-MacBook-Pro.local>
2023-01-14 10:22:33 -0800

    get appropriate features

---
+++ src/feature_engineering.py
@@ -0,0 +1,121 @@
+import pandas as pd
+import numpy as np
+
+
+weeks_used = 8
+raw_files = ["data/week" + str(i) + ".csv" for i in range(1, weeks_used + 1)]
+pass_rush_files = [f"data/sample_data_week_{i}.csv" for i in range(weeks_used)]
+
+
+def generate_acceleration_data():
+    """
+    generates acceleration towards qb in y direction hmm data
+    """
+    data_list = []
+    for raw_file, pass_rush_file in zip(raw_files, pass_rush_files):
+        data = pd.read_csv(raw_file)
+        processed_week_data = pd.read_csv(pass_rush_file)
+
+        ### get timesteps between snap and first event
+        event_instances = (
+            data.groupby(["gameId", "playId"])
+            .apply(
+                lambda x: x[x.event != "None"].drop_duplicates(["time"])[
+                    ["time", "event"]
+                ]
+            )
+            .reset_index()
+        )
+        event_timestamp = (
+            event_instances.groupby(["gameId", "playId"])
+            .apply(
+                lambda x: x[
+                    x.event.isin(
+                        ["pass_forward", "run", "qb_sack", "qb_strip_sack", "ball_snap"]
+                    )
+                ][["time", "event"]]
+            )
+            .reset_index()
+        )
+        event_timestamp["event"] = event_timestamp["event"].apply(
+            lambda x: "action" if x != "ball_snap" else x
+        )
+        event_timestamp["time"] = pd.to_datetime(event_timestamp["time"])
+        event_wide = pd.pivot_table(
+            event_timestamp[["gameId", "time", "playId", "event"]],
+            columns="event",
+            values=["time"],
+            index=["gameId", "playId"],
+        ).reset_index()
+        event_wide.columns = ["gameId", "playId", "action", "ball_snap"]
+
+        ### now merge
+        merged_event_processed = pd.merge(event_wide, processed_week_data)
+        filtered_processed = merged_event_processed[
+            (
+                pd.to_datetime(merged_event_processed.time)
+                <= merged_event_processed.action
+            )
+            & (
+                pd.to_datetime(merged_event_processed.time)
+                >= merged_event_processed.ball_snap
+            )
+        ]
+        pr_filtered = filtered_processed
+
+        #### create physics features
+        pr_filtered["dx_pr"] = pr_filtered.groupby(
+            ["gameId", "playId", "nflId_pr", "nflId"]
+        )["x_pr"].transform(lambda x: np.gradient(x, 0.1))
+        pr_filtered["dy_pr"] = pr_filtered.groupby(
+            ["gameId", "playId", "nflId_pr", "nflId"]
+        )["y_pr"].transform(lambda x: np.gradient(x, 0.1))
+        pr_filtered["dx_qb"] = pr_filtered.groupby(
+            ["gameId", "playId", "nflId_pr", "nflId"]
+        )["x_qb"].transform(lambda x: np.gradient(x, 0.1))
+        pr_filtered["dy_qb"] = pr_filtered.groupby(
+            ["gameId", "playId", "nflId_pr", "nflId"]
+        )["y_qb"].transform(lambda x: np.gradient(x, 0.1))
+        pr_filtered["x_pr_qb"] = pr_filtered["x_qb"] - pr_filtered["x_pr"]
+        pr_filtered["y_pr_qb"] = pr_filtered["y_qb"] - pr_filtered["y_pr"]
+        pr_filtered["d2y_pr"] = pr_filtered.groupby(
+            ["gameId", "playId", "nflId_pr", "nflId"]
+        )["dy_pr"].transform(lambda x: np.gradient(x, 0.1))
+        pr_filtered["d2x_pr"] = pr_filtered.groupby(
+            ["gameId", "playId", "nflId_pr", "nflId"]
+        )["dx_pr"].transform(lambda x: np.gradient(x, 0.1))
+        pr_filtered["pr_qb_norm"] = (
+            pr_filtered["x_pr_qb"] ** 2 + pr_filtered["y_pr_qb"] ** 2
+        )
+        pr_filtered["scalar_projection"] = (
+            pr_filtered["x_pr_qb"] * pr_filtered["d2x_pr"]
+            + pr_filtered["y_pr_qb"] * pr_filtered["d2y_pr"]
+        ) / pr_filtered["pr_qb_norm"]
+        pr_filtered["d2y_pr_qb"] = (
+            pr_filtered["scalar_projection"] * pr_filtered["y_pr_qb"]
+        )
+        pr_filtered["d2x_pr_qb"] = (
+            pr_filtered["scalar_projection"] * pr_filtered["x_pr_qb"]
+        )
+
+        ### calculate strain
+        pr_filtered["d_ij"] = np.sqrt(
+            np.square(pr_filtered["x_pr"] - pr_filtered["x_qb"])
+            + np.square(pr_filtered["y_pr"] - pr_filtered["y_qb"])
+        )
+        pr_filtered["strain_rate"] = pr_filtered.groupby(
+            ["gameId", "playId", "nflId_pr", "nflId"]
+        )["d_ij"].transform(lambda x: -np.gradient(x, 0.1))
+        pr_filtered["strain_rate"] /= pr_filtered["d_ij"]
+        pr_filtered["strain_acceleration"] = pr_filtered.groupby(
+            ["gameId", "playId", "nflId_pr", "nflId"]
+        )["strain_rate"].transform(lambda x: np.gradient(x, 0.1))
+
+        ### append to list
+        data_list.append(pr_filtered)
+
+    pd.concat(data_list).to_csv("processed_data/pass_rusher_features.csv", index=False)
+
+
+if __name__ == "__main__":
+    generate_acceleration_data()
